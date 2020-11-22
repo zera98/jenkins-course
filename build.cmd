44c0bca9613b6ad2d61facb985f8c5082a953e51
rtrk-jenkins-course <74553300+rtrk-jenkins-course@users.noreply.github.com>
2020-11-22 10:32:55 +0100

    Added main build script
    Added main build script

---
+++ build.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+echo "Build started"
+ping -n 3 127.0.0.1 > nul
+echo "Compiling C libs ..."
+ping -n 3 127.0.0.1 > nul
+echo "Compiling modules ..."
+ping -n 3 127.0.0.1 > nul
+echo "Build completed successfully."
