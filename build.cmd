87cc384289fe6ba92a9b9355ebb356bd0d91666d
KLevon <42465755+KLevon@users.noreply.github.com>
2024-04-03 02:54:38 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -7,3 +7,5 @@ ping -n 3 127.0.0.1 > nul
 echo "Compiling modules ..."
 ping -n 3 127.0.0.1 > nul
 echo "Build completed successfully."
+echo "Some library 1" > app_lib_1.a
+echo "Some library 2" > app_lib_2.a
